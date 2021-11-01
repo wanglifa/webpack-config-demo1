42baf12247c79fd8ac54cf23c43a90addbb6b4e6
wanglifa <wanglifa1995@gmail.com>
2021-11-01 14:43:49 +0800

    chore: 支持ie

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-const str = 'hi'
-console.log(str)
+import {a} from './a' // 直接引入
+const b = import('./b') // 动态引入，也按需加载
+
+const hi = () => {
+  console.log('frank')
+  console.log(a)
+  console.log(b)
+  console.log(Promise.resolve('test promise'))
+}
+
+hi()
