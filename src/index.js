b6685cd6a32e17f6a6d749056125b354e58f3c52
wanglifa <wanglifa1995@gmail.com>
2021-11-02 12:32:49 +0800

    chore: JS 可以获取 LESS 变量

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { JsxDemo } from "./jsx-demo.jsx";
 import { x } from './ts-demo.ts'
 import {TsxDemo} from "./tsx-demo.tsx";
 import './scss-demo.scss'
-import vars from '@/scss-export.scss'
-import '@/less-demo.less'
+import vars from '@/less-demo.less'
+
 console.log(vars)
 const b = import('./b') // 动态引入，也按需加载
 console.log(x)
