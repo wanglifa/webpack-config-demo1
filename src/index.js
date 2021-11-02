45c67a705e64948721da098bccc0c7d8b29183cc
wanglifa <wanglifa1995@gmail.com>
2021-11-02 13:17:01 +0800

    chore: 支持 STYL

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { JsxDemo } from "./jsx-demo.jsx";
 import { x } from './ts-demo.ts'
 import {TsxDemo} from "./tsx-demo.tsx";
 import './scss-demo.scss'
-import vars from '@/less-demo.less'
-
+import '@/stylus-demo.styl'
+import vars from '@/stylus-vars.styl'
 console.log(vars)
 const b = import('./b') // 动态引入，也按需加载
 console.log(x)
