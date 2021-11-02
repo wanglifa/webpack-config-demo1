a37426bd4a5fb1cd8505666a733824a1886a3d44
wanglifa <wanglifa1995@gmail.com>
2021-11-02 11:40:12 +0800

    chore: js 可以读取 scss 变量

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { JsxDemo } from "./jsx-demo.jsx";
 import { x } from './ts-demo.ts'
 import {TsxDemo} from "./tsx-demo.tsx";
 import './scss-demo.scss'
-
+import vars from '@/scss-export.scss'
+console.log(vars)
 const b = import('./b') // 动态引入，也按需加载
 console.log(x)
 console.log(JsxDemo)
